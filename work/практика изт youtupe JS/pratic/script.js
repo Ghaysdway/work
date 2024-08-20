1d06925bd697871536ff8f560a2ad449464df6ae
ghays <furore-plus@ukr.net>
2024-08-20 13:12:10 +0200

    added work directory

---
+++ work/практика изт youtupe JS/pratic/script.js
@@ -0,0 +1,11 @@
+const  userName = {
+    visible: true,
+    colorDepth: 24,
+    title: 'my Image',
+    orientation: {
+        angle: 0,
+        type: 'landspace',
+    }
+    pixeldepth: 24,
+    width : 1440
+}
